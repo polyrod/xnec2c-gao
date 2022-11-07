17b993e7b5bad6e14184bc195426b6e005334eae
Maurizio Di Pietro <dc1mdp@gmail.com>
2022-11-07 18:33:28 +0100

    scoreing code adjustments

--- app/OptFunc.hs
+++ app/OptFunc.hs
@@ -7,8 +7,13 @@ optFunc VSWR dm = \(Fitness s _ f) -> swr s + fbr dm f
 optFunc GAIN dm = \(Fitness _ g f) -> gain g + fbr dm f
 optFunc VSWRGAIN dm = \(Fitness s g f) -> swr s + gain g + fbr dm f
 
+swrMaxThrs :: Float
+swrMaxThrs = 1.5
+
 swr :: Float -> Float
-swr s = 100 / s
+swr s
+  | s < swrMaxThrs = (swrMaxThrs - s) * 100 + (100/s)
+  | otherwise = 100 / s
 
 gain :: Float -> Float
 gain g = 30 * g
@@ -18,4 +23,4 @@ fbr SYMMETRICAL 0 = 1000
 fbr SYMMETRICAL f
   | f < 0.3 = 100 / 0.3 + (f * f)
   | otherwise = 10 / f
-fbr DIRECTIVE f = 10 * f
+fbr DIRECTIVE f = f
